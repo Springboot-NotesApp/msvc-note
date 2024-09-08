76120b3dad900879682e9ef0ad4e49a0b321b3c9
AngelTravieso <angeltraviesoc@gmail.com>
2024-09-08 01:25:57 -0400

    Initial Commit

---
+++ src/main/java/com/at/notes/msvc_notes/MsvcNotesApplication.java
@@ -0,0 +1,13 @@
+package com.at.notes.msvc_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/at/notes/msvc_notes/MsvcNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.at.notes.msvc_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
