df6048125c13268338260c3b08c19400fdce3cae
AngelTravieso <angeltraviesoc@gmail.com>
2024-09-09 08:48:08 -0400

    CHANGE - Note model attributes

--- src/main/java/com/at/notes/msvc_notes/models/Note.java
+++ src/main/java/com/at/notes/msvc_notes/models/Note.java
@@ -1,29 +1,30 @@
 package com.at.notes.msvc_notes.models;
 
-import com.at.notes.msvc_notes.enums.NoteStatus;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
-@Document("Notes")
+@Document(collection = "Notes")
+@Data
 public class Note {
     @Id
     private String id;
     private String title;
     private String description;
-//    private NoteStatus status;
-//    private LocalDateTime createdAt;
-//    private Optional<LocalDateTime> updatedAt;
 
     public Note(String id, String title, String description) {
         super();
         this.id = id;
         this.title = title;
         this.description = description;
-//        this.status = status;
-//        this.createdAt = LocalDateTime.now();
-//        this.updatedAt = Optional.empty();
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
