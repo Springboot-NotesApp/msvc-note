717664b0a5818590e105f482d0c3bf79bf26f576
AngelTravieso <angeltraviesoc@gmail.com>
2024-09-08 21:23:59 -0400

    REMOVE - /api prefix

--- src/main/java/com/at/notes/msvc_notes/controllers/NoteController.java
+++ src/main/java/com/at/notes/msvc_notes/controllers/NoteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/notes")
+@RequestMapping("/notes")
 public class NoteController {
 
     @Autowired
